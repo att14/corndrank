228e1262d82ff1dba07fe30f336e09c17bd0f540
Andrew Tribone <tribone@easypost.com>
2020-03-18 19:04:56 -0700

    No.

---
+++ corndrank/app.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import (
+    Flask,
+    render_template,
+    send_from_directory,
+)
+
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', None)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon',
+    )
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
